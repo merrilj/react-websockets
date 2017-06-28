ab0f187df1e984214cf8641e8b7921c9e052c424
Merril Jeffs <merril@Merrils-MacBook-Pro.local>
2017-06-28 10:04:55 -0600

    implemented websockets

--- react-websockets/src/App.js
+++ react-websockets/src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
+import Websocket from 'react-websocket';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 90
+    };
+  }
+
+  handleData(data) {
+    let result = JSON.parse(data);
+    this.setState({count: this.state.count + result.movement})
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React With WebSockets</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          Count: <strong>{this.state.count}</strong>
+        <Websocket url='ws://localhost:8888/live/product/12345/' onMessage={this.handleData.bind(this)} />
+        </div>
       </div>
     );
   }
